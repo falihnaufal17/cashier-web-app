a687c00955771f257ee2e6844b2ba44879510e17
falihnaufal17 <dudu.kotkot@gmail.com>
2019-09-06 07:05:46 +0700

    no header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Axios from 'axios';
 
 export class App extends Component {
   render() {
-    Axios.defaults.headers.common['authorization'] = 'x-cashier-app'
     return (
       <Routing />
     );
